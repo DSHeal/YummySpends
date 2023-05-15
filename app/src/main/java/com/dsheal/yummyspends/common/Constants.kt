8581f8d58fad1aed88f49eb3614b8f9ac2b5c3a4
daria_khilova <daryamilkova@gmail.com>
2023-05-15 20:17:47 +0300

    Constants changed

--- app/src/main/java/com/dsheal/yummyspends/common/Constants.kt
+++ app/src/main/java/com/dsheal/yummyspends/common/Constants.kt
@@ -4,9 +4,6 @@ object Constants {
 
     const val APP_DATABASE = "app_database"
 
-    const val YS_SHEET_TABLE_ID = "1Cw8E8ZXlvZPtGdWWXUs0_HpBdnY7bilb3MGYMcntzCU"
-    const val GOOGLE_SHEETS_API_KEY = "AIzaSyDTbUDWduSAh4g7vRbNr5IcT6EKND9XzrI"
-
     const val BASE_GOOGLE_SHEETS_URL = "https://sheets.googleapis.com"
-    const val YUMMY_SPENDS_FIREBASE_DB_URL = "https://yummyspends-default-rtdb.europe-west1.firebasedatabase.app/"
+    const val YUMMY_SPENDS_FIREBASE_DB_URL = "Your Firebase Database URL"
 }
